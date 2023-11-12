cc0c67ab0e49ac4fd4c0439cf309d95969ffe099
Oleg <Oleg-BLR@yandex.ru>
2023-11-12 08:52:11 +0300

    Task6 done

---
+++ src/com/teachmeskills/lesson04/additionaltasks/Task6.java
@@ -0,0 +1,11 @@
+package com.teachmeskills.lesson04.additionaltasks;
+
+import java.util.Arrays;
+
+public class Task6 {
+    public static void main(String[] args) {
+        String[] names = {"Misha", "Max", "Gleb", "Vera", "Stas"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+    }
+}
